90ee5aa29e8978de2a1b1a8dcb3b1b512ee288c1
Satwik <97330754+satwikgm@users.noreply.github.com>
2024-04-21 19:27:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+document.getElementById("urlForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var name = document.getElementById("name").value;
+  var year = document.getElementById("year").value;
+
+  var queryString = "?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+  document.getElementById("url").textContent = "https://localhost:8080/" + queryString;
+});
